fd3ca0a776998cdce76b2350bac9508782a6cf67
PES1UG20CS005 <125341497+PES1UG20CS005@users.noreply.github.com>
2023-02-16 14:02:46 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "This is Task 4\n";
+  cout << "I have made changes\n";
   return 0;
 }
